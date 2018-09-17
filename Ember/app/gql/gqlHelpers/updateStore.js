28e0c47d8845c7fb25b44febcb83ee2daf30d592
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-09-17 14:42:07 -0500

    Added helpers to add and remove from the apollo store when user edits and deletes data

---
+++ Ember/app/gql/gqlHelpers/updateStore.js
@@ -0,0 +1,85 @@
+
+function updateRecordFromDetail(currentStore, detailQuery, recordKey, updatedData, objToUpdateName){
+  //update the local store
+  //Set the variables, not how this matches the actual variable in the mutation
+  let updateVars = { _key: recordKey };
+  //Get the existing query from the store and replace the data
+  //note the try catch is the only way to check if the query in
+  //already in the store;
+  try {
+    const data = currentStore.readQuery({ query: detailQuery, variables: updateVars });
+    data[objToUpdateName].pop();
+    data[objToUpdateName].push(updatedData);
+    //Update the store only if the query was already there,
+    //otherwise it will be automatically fetched later
+    currentStore.writeQuery({ query: detailQuery, data, variables: updateVars})
+  } catch (e) {
+    // Do nothing
+  }
+}
+
+
+function updateRecordFromSelection(currentStore, selectionQuery, recordKey, updatedData, objToUpdateName){
+  //update the local store
+  //Get the existing query from the store and replace the data
+  //note the try catch is the only way to check if the query in
+  //already in the store;
+  try {
+    const data = currentStore.readQuery({ query: selectionQuery });
+    var index = data[objToUpdateName].map(x =>  x._key ).indexOf(recordKey);
+    data[objToUpdateName].splice(index, 1, updatedData);
+    //Update the store only if the query was already there,
+    //otherwise it will be automatically fetched later
+    currentStore.writeQuery({ query: selectionQuery, data})
+  } catch (e) {
+    // Do nothing
+  }
+}
+
+
+
+//// TODO: write the patch function for partial updates
+function patchRecordFromDetail(){
+  let i = 1;
+}
+
+function removeRecordFromDetail(currentStore, detailQuery, recordKey, objToUpdateName){
+  //update the local store
+  //Set the variables, not how this matches the actual variable in the mutation
+  let updateVars = { _key: recordKey };
+  try {
+    const dataCurrent = currentStore.readQuery({ query: detailQuery, variables: updateVars });
+     dataCurrent[objToUpdateName].pop();
+    //Update the store only if the query was already there,
+    //otherwise it will be automatically fetched later
+    currentStore.writeQuery({ query: detailQuery, data: dataCurrent, variables: updateVars})
+  } catch (e) {
+    // Do nothing
+  }
+
+}
+
+function removeRecordFromSelection(currentStore, selectionQuery, recordKey, objToUpdateName){
+  //Get the existing query from the store and replace the data
+  //note the try catch is the only way to check if the query in
+  //already in the store;
+  try {
+    // Find record in the store for the parent query  and remove it
+    //so user does not manually have to refresh
+    const data = currentStore.readQuery({ query: selectionQuery });
+    var index = data[objToUpdateName].map(x =>  x._key ).indexOf(recordKey);
+    data[objToUpdateName].splice(index, 1);
+    //Update the store
+    currentStore.writeQuery({ query: selectionQuery, data})
+  } catch (e) {
+    // Do nothing
+  }
+
+}
+
+export default  {
+  updateRecordFromDetail,
+  updateRecordFromSelection,
+  removeRecordFromDetail,
+  removeRecordFromSelection
+}
--- Ember/app/routes/cust-kpi-formulas/show.js
+++ Ember/app/routes/cust-kpi-formulas/show.js
@@ -5,6 +5,7 @@ import query from 'ember-gui/gql/queries/custKpiFormula/custKpiFormula';
 import mutation from 'ember-gui/gql/mutations/custKpiFormula/custKpiFormulaPut';
 import mutationDeleteFull from 'ember-gui/gql/mutations/custKpiFormula/custKpiFormulaDeleteFull';
 import queryParent from 'ember-gui/gql/queries/custKpiFormula/custKpiFormulas';
+import UpdateStore from 'ember-gui/gql/gqlHelpers/updateStore';
 // import { inject as service } from "@ember/service";
 
 
@@ -27,22 +28,25 @@ export default Route.extend(RouteQueryManager,{
       //Update the backend
       return this.get("apollo").mutate({mutation, variables,
         update: (store, mutationResult) => {
-          //update the local store
-          //Set the variables, not how this matches the actual variable in the mutation
-          let updateVars = { _key: key };
-          //Get the existing query from the store and replace the data
-          //note the try catch is the only way to check if the query in
-          //already in the store;
-          try {
-            const data = store.readQuery({ query, variables: updateVars });
-            data.custKpiFormula.pop();
-            data.custKpiFormula.push(mutationResult.data.custKpiFormulaPut);
-            //Update the store only if the query was already there,
-            //otherwise it will be automatically fetched later
-            store.writeQuery({ query, data, variables: updateVars})
-          } catch (e) {
-            // Do nothing
-          }
+
+          UpdateStore.updateRecordFromDetail(store, query, key, mutationResult.data.custKpiFormulaPut,"custKpiFormula")
+          UpdateStore.updateRecordFromSelection(store, queryParent, key, mutationResult.data.custKpiFormulaPut, "custKpiFormulas")
+          // //update the local store
+          // //Set the variables, not how this matches the actual variable in the mutation
+          // let updateVars = { _key: key };
+          // //Get the existing query from the store and replace the data
+          // //note the try catch is the only way to check if the query in
+          // //already in the store;
+          // try {
+          //   const data = store.readQuery({ query, variables: updateVars });
+          //   data.custKpiFormula.pop();
+          //   data.custKpiFormula.push(mutationResult.data.custKpiFormulaPut);
+          //   //Update the store only if the query was already there,
+          //   //otherwise it will be automatically fetched later
+          //   store.writeQuery({ query, data, variables: updateVars})
+          // } catch (e) {
+          //   // Do nothing
+          // }
 
         }
       }, "custKpiFormula")
@@ -59,24 +63,39 @@ export default Route.extend(RouteQueryManager,{
         update: (store, mutationResult) => {
           //update the local store
           //Set the variables, not how this matches the actual variable in the mutation
-          let updateVars = { _key: key };
-          //Get the existing query from the store and replace the data
-          //note the try catch is the only way to check if the query in
-          //already in the store;
-          try {
-            // Finf record in the store for the parent query  and remove it
-            //so user does not manually have to refresh
-            const data = store.readQuery({ query: queryParent });  
-            var index = data.custKpiFormulas.map(x =>  x._key ).indexOf(key);
-            data.custKpiFormulas.splice(index, 1);
-            //Update the store
-            store.writeQuery({ query: queryParent, data})
-          } catch (e) {
-            // Do nothing
-          }
+          // let updateVars = { _key: key };
+          // try {
+          //   const dataCurrent = store.readQuery({ query, variables: updateVars });
+          //   dataCurrent.custKpiFormula.pop();
+          //   //Update the store only if the query was already there,
+          //   //otherwise it will be automatically fetched later
+          //   store.writeQuery({ query, data: dataCurrent, variables: updateVars})
+          // } catch (e) {
+          //   // Do nothing
+          // }
+          UpdateStore.removeRecordFromDetail(store, query, key, "custKpiFormula")
+          UpdateStore.removeRecordFromSelection(store, queryParent, key, "custKpiFormulas")
+
+          // //Get the existing query from the store and replace the data
+          // //note the try catch is the only way to check if the query in
+          // //already in the store;
+          // try {
+          //   // Find record in the store for the parent query  and remove it
+          //   //so user does not manually have to refresh
+          //   const data = store.readQuery({ query: queryParent });
+          //   var index = data.custKpiFormulas.map(x =>  x._key ).indexOf(key);
+          //   data.custKpiFormulas.splice(index, 1);
+          //   //Update the store
+          //   store.writeQuery({ query: queryParent, data})
+          // } catch (e) {
+          //   // Do nothing
+          // }
 
         }
       })
+      // .then( () => {
+      //   this.transitionTo('custKpiFormulas');
+      // })
 
     }
   }

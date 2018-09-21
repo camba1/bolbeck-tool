7499fe1440d21f867a1f9a41267309c645f82cb0
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-09-20 21:57:36 -0500

    Additional testing logic for custKpiFormulas

--- Ember/tests/acceptance/cust-kpi-formulas-test.js
+++ Ember/tests/acceptance/cust-kpi-formulas-test.js
@@ -1,8 +1,10 @@
 import { module, test } from 'qunit';
-import { visit, currentURL, find , click, settled, fillIn} from '@ember/test-helpers';
+import { visit, currentURL, find , click, settled, fillIn, findAll} from '@ember/test-helpers';
 import { setupApplicationTest } from 'ember-qunit';
 
 let document = { _key: "testEmber1", name : "testEmber1", formula: "formula1", type : "type1", validityDate : "1999-01-01", calculationOrder : 1 }
+const documentUpdate = { formula : "formula2"};
+const documentUpdate2 = { type: "type2"};
 
 module('Acceptance | cust kpi formulas', function(hooks) {
   setupApplicationTest(hooks);
@@ -14,7 +16,7 @@ module('Acceptance | cust kpi formulas', function(hooks) {
   });
 
 
-  test('visiting /cust-kpi-formulas', async function(assert) {
+  test('visiting /cust-kpi-formulas/new', async function(assert) {
     // let apollo = this.owner.lookup('service:apollo');
     // let getQueries = () => apollo.client.queryManager.queryStore.getStore();
 
@@ -26,7 +28,7 @@ module('Acceptance | cust kpi formulas', function(hooks) {
     await settled();
     assert.equal(currentURL(), '/cust-kpi-formulas/new', 'Transition to new record page');
 
-    //debugger;
+    // Create a new record
     let currentModel = this.owner.lookup('route:cust-kpi-formulas/new').modelFor('cust-kpi-formulas.new');
 
     await fillIn('.modelName', document.name);
@@ -38,7 +40,82 @@ module('Acceptance | cust kpi formulas', function(hooks) {
     assert.notOk(currentModel._key, 'Verify the key of the record is empty');
     await click('.saveNewDataBtn');
     assert.ok(currentModel._key,'Key is populated after save');
+    assert.equal(currentModel.formula, document.formula, 'Formula is still in model and matches our input')
     document._key = currentModel._key;
 
+    // Update a 'new' record
+    await fillIn('.modelFormula', documentUpdate.formula);
+    await click('.updateDataBtn');
+    await click('.cancelBtn');
+
+    assert.equal(currentURL(), '/cust-kpi-formulas', 'Transition to selection page');
+    await settled();
+    
+    //The new record should be the first in the listing on the store
+    let selectionModel = this.owner.lookup('route:cust-kpi-formulas').modelFor('cust-kpi-formulas.index');
+    assert.equal(selectionModel.length > 0, true, 'The selection screen model has data: '.concat(selectionModel.length));
+    assert.equal(findAll('.listing').length > 0, true, 'We have the data showing '.concat(findAll('.listing').length));
+    let keyFoundinDoc = find('.detail.key').innerText.split(' ')
+                              .reduce((total, x) => {
+                              total = ((x == document._key) ? total + 1 : total); return total
+                              } , 0);
+
+    assert.equal(keyFoundinDoc, 1, 'The document that was inserted in first place');
+    // assert.ok(find('.formula').innerText.includes(documentUpdate.formula), 'The document that was updated with the correct formula');
+    let formulaFoundinDoc = find('.detail.formula').innerText.split(' ')
+                              .reduce((total, x) => {
+                              total = ((x == documentUpdate.formula) ? total + 1 : total); return total
+                              } , 0);
+    assert.equal(formulaFoundinDoc, 1, 'The document was updated with the correct formula');
+//  });
+
+//  test('visiting /cust-kpi-formulas/show', async function(assert) {
+
+
+    // Testing show page
+
+    // Update record
+
+    await visit('/cust-kpi-formulas/'.concat(document._key));
+    await settled();
+
+    let detailModel = this.owner.lookup('route:cust-kpi-formulas/show').modelFor('cust-kpi-formulas.show');
+    assert.ok(detailModel, 'found model');
+
+    let keyFoundinChildDoc = find('.detail.key').innerText.split(' ')
+                              .reduce((total, x) => {
+                              total = ((x == document._key) ? total + 1 : total); return total
+                              } , 0);
+
+    assert.equal(keyFoundinChildDoc, 1, 'The document has the correct key');
+    await fillIn('.modelType', documentUpdate2.type);
+    await click('.updateDataBtn');
+    await click('.backBtn');
+
+    assert.equal(currentURL(), '/cust-kpi-formulas', 'Transition to selection page');
+    await settled();
+
+
+    // Delete record
+
+    await visit('/cust-kpi-formulas/'.concat(document._key));
+    await settled();
+
+    assert.equal(find('.modelType').value, documentUpdate2.type, 'Type was updated properly '.concat(currentURL()));
+
+    await click('.deleteDataBtn');
+    await settled();
+
+    assert.equal(currentURL(), '/cust-kpi-formulas', 'Transition to selection page after record delete');
+    await settled();
+
+    await visit('/cust-kpi-formulas/'.concat(document._key));
+    await settled();
+
+    detailModel = this.owner.lookup('route:cust-kpi-formulas/show').modelFor('cust-kpi-formulas.show');
+
+    assert.equal(detailModel.length, 0, 'Document deleted properly');
+
   });
+
 });

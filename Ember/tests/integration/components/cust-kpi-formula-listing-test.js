5a4421d66b53218868e56f6070ebb07d5ec53d06
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-10-04 15:01:40 -0500

    Adjusted component and its test to account for new table layout

--- Ember/tests/integration/components/cust-kpi-formula-listing-test.js
+++ Ember/tests/integration/components/cust-kpi-formula-listing-test.js
@@ -8,14 +8,14 @@ module('Integration | Component | cust-kpi-formula-listing', function(hooks) {
   setupRenderingTest(hooks);
 
   hooks.beforeEach(function() {
-    this.custKpiFormula = EmberObject.create({
+    this.custKpiFormula = [EmberObject.create({
       _key: '123',
       name: 'Test',
       formula: 'x + y',
       type: 'Quantity',
       validityDate: '01/01/2001',
       calculationOrder: 2
-    });
+    })];
   });
 
   // test('it renders', async function(assert) {
@@ -37,9 +37,15 @@ module('Integration | Component | cust-kpi-formula-listing', function(hooks) {
   // });
 
   test('should display Kpi formula details', async function(assert) {
-    await render(hbs`{{cust-kpi-formula-listing custKpiFormula=custKpiFormula}}`);
-    assert.equal(this.element.querySelector('.listing .key').textContent.trim(),'Key: 123', 'Key 123');
-    assert.equal(this.element.querySelector('.listing .formula').textContent.trim(),'Formula: x + y', 'Formula: x + y');
+    // await render(hbs`{{cust-kpi-formula-listing custKpiFormula=custKpiFormula}}`);
+    //textContent
+    await render(hbs`{{cust-kpi-formula-listing custKpiFormula = custKpiFormula}}`)
+    let rows = this.element.querySelector('.table .mybody').rows;
+    let key = rows[0].getElementsByClassName('key')[0].textContent;
+    let formula = rows[0].getElementsByClassName('formula')[0].textContent
+    assert.equal(rows.length, 1, "the table had " + rows + " rows");
+    assert.equal(key,'123', 'Key 123');
+    assert.equal(formula, 'x + y', 'Formula: x + y');
   })
 
 });

c34dda5976aa87150f91672a0618a8f361b2ab9f
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-10-17 16:13:51 -0500

    removed unused params var

--- Ember/app/routes/products/show/show-detail.js
+++ Ember/app/routes/products/show/show-detail.js
@@ -1,7 +1,7 @@
 import Route from '@ember/routing/route';
 
 export default Route.extend({
-  model(params) {
+  model() {
     let mymodel = this.modelFor('products.show');
     return mymodel;
   }

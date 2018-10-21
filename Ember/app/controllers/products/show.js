0f0ea27ed51b40c141383a5788f00f62f31db635
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-10-20 19:16:48 -0500

    Hide some tabs if the product is not a sku

--- Ember/app/controllers/products/show.js
+++ Ember/app/controllers/products/show.js
@@ -1,4 +1,8 @@
 import Controller from '@ember/controller';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+  isSku: computed('model', function(){
+     return this.model[0].hierarchyLevel == 'sku';
+   s})
 });

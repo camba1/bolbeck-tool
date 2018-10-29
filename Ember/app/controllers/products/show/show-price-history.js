cc244d19834541b639e971efed6a4073b27c85ef
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-10-29 10:01:23 -0500

    changed sample table

--- Ember/app/controllers/products/show/show-price-history.js
+++ Ember/app/controllers/products/show/show-price-history.js
@@ -5,7 +5,7 @@ export default Controller.extend({
     this._super(...arguments);
     this.chartLayout = {title: 'Price History'};
     //this.chartOptions = {displayModeBar: false};
-    // this.findata = [
+    // this.basicData = [
     //   {
     //   x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
     //   y: [1, 3, 6],
--- Ember/app/routes/products/show/show-price-history.js
+++ Ember/app/routes/products/show/show-price-history.js
@@ -4,13 +4,15 @@ import Route from '@ember/routing/route';
 export default Route.extend({
     model(){
       return [{
-        x: [1,2,3],
-        y: [2,4,6],
-        type: 'scatter'
+          x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+          y: [1, 3, 6],
+          type: 'scatter',
+          name: 'Price'
       },{
-        x: [0,10],
-        y: [10,-10],
-        type: 'scatter'
+        x: ['2013-10-10 22:23:00', '2013-11-14 22:23:00', '2013-12-30 22:23:00'],
+        y: [3, 2, 1.5],
+        type: 'scatter',
+        name: 'Cost'
       }]
     }
 

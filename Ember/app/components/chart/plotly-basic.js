16b1b9e4a560351ea1a01ff963168e3072bd8f33
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-10-29 15:31:13 -0500

    Changes to enable remaining charts to pass information to the parentUI

--- Ember/app/components/chart/plotly-basic.js
+++ Ember/app/components/chart/plotly-basic.js
@@ -1,8 +1,25 @@
 import Component from '@ember/component';
 import Plotly from 'plotly.js-basic-dist';
 
-
+/**
+ * Component to add plotly charts to application. Typical call:
+   ```hbs
+   {{chart/plotly-finance
+     chartData=model
+     chartLayout=chartLayout
+     chartOptions = chartOptions
+     chartEvent=(action "chartEvent")
+   }}
+   ```
+ */
 export default Component.extend({
+  //rethrow the event but pass only the necesary information
+  onPlotlyEvent: function(eventName, points){
+    if (this.chartEvent) {
+      let clickedPoints = points ? points.points : undefined
+      this.chartEvent(eventName, clickedPoints);
+    }
+  },
   didInsertElement() {
     const data = this.chartData ? this.chartData : [] ;
     const layout = this.chartLayout ? this.chartLayout : {};
@@ -18,6 +35,36 @@ export default Component.extend({
                   responsive: true,
                   modeBarButtonsToRemove: ['sendDataToCloud']};
     }
-    Plotly.newPlot(this.elementId, data, layout, options);
+    removePlotlyEvents(this);
+    Plotly.newPlot(this.elementId, data, layout, options).then(() => {
+      setupPlotlyEvents(this)
+    });
   }
 });
+
+/**
+ * List of events that this component will push up to the calling UI
+ */
+const selectedPlotlyEvents = [ 'click', 'legendclick', 'selected'].map(suffix => `plotly_${suffix}`);
+
+/**
+ * Setup listeners for events to the sent up to the calling UI
+ * @param {object} mythis Reference to 'this' which in this case is the component
+ */
+function setupPlotlyEvents(mythis){
+  selectedPlotlyEvents.forEach((eventName) => {
+    mythis.element.on(eventName, (...args) => mythis.onPlotlyEvent(eventName, ...args));
+  })
+}
+/**
+ * Remove listeners for events to the sent up to the calling UI
+ * @param {object} mythis Reference to 'this' which in this case is the component
+ */
+function removePlotlyEvents(mythis) {
+     selectedPlotlyEvents.forEach(eventName => {
+       // Note: Using plotly.js' 'removeListener' method (copied from EventEmitter)
+       if (typeof mythis.element.removeListener === 'function') {
+         mythis.element.removeListener(eventName, mythis.onPlotlyEvent);
+       }
+     });
+   }
--- Ember/app/components/chart/plotly-geo.js
+++ Ember/app/components/chart/plotly-geo.js
@@ -4,9 +4,27 @@
 import Component from '@ember/component';
 import Plotly from 'plotly.js-geo-dist';
 
+/**
+ * Component to add plotly charts to application. Typical call:
+   ```hbs
+   {{chart/plotly-finance
+     chartData=model
+     chartLayout=chartLayout
+     chartOptions = chartOptions
+     chartEvent=(action "chartEvent")
+   }}
+   ```
+ */
 export default Component.extend({
   // attributeBindings: ['customid:id'],
   // customid: 'geoplot',
+  //rethrow the event but pass only the necesary information
+  onPlotlyEvent: function(eventName, points){
+    if (this.chartEvent) {
+      let clickedPoints = points ? points.points : undefined
+      this.chartEvent(eventName, clickedPoints);
+    }
+  },
   didInsertElement() {
     // Plotly.d3.csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_february_us_airport_traffic.csv', function(err, rows){
     //
@@ -80,6 +98,36 @@ export default Component.extend({
                   responsive: true,
                   modeBarButtonsToRemove: ['sendDataToCloud']};
     }
-    Plotly.newPlot(this.elementId, data, layout, options);
+    removePlotlyEvents(this);
+    Plotly.newPlot(this.elementId, data, layout, options).then(() => {
+      setupPlotlyEvents(this)
+    });
   }
 });
+
+/**
+ * List of events that this component will push up to the calling UI
+ */
+const selectedPlotlyEvents = [ 'click', 'legendclick', 'selected'].map(suffix => `plotly_${suffix}`);
+
+/**
+ * Setup listeners for events to the sent up to the calling UI
+ * @param {object} mythis Reference to 'this' which in this case is the component
+ */
+function setupPlotlyEvents(mythis){
+  selectedPlotlyEvents.forEach((eventName) => {
+    mythis.element.on(eventName, (...args) => mythis.onPlotlyEvent(eventName, ...args));
+  })
+}
+/**
+ * Remove listeners for events to the sent up to the calling UI
+ * @param {object} mythis Reference to 'this' which in this case is the component
+ */
+function removePlotlyEvents(mythis) {
+     selectedPlotlyEvents.forEach(eventName => {
+       // Note: Using plotly.js' 'removeListener' method (copied from EventEmitter)
+       if (typeof mythis.element.removeListener === 'function') {
+         mythis.element.removeListener(eventName, mythis.onPlotlyEvent);
+       }
+     });
+   }

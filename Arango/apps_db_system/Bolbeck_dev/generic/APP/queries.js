9f8aa9bc10e6d6345246c2a1ce55b425896b17a1
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-11-13 11:02:14 -0600

    Modify collection get method to use query params when available

--- Arango/apps_db_system/Bolbeck_dev/generic/APP/queries.js
+++ Arango/apps_db_system/Bolbeck_dev/generic/APP/queries.js
@@ -13,14 +13,49 @@ const HTTP_CONFLICT = status('conflict');
 
 const g_MaxRecordsByQuery = 20;
 
-// Get a number of documents from a given collection
-var genericCollectionGet = function (collection) {
-  const documents = db._query(aql`
-    FOR entry IN ${collection}
-    FILTER entry.status != "d"
-    LIMIT ${g_MaxRecordsByQuery}
-    RETURN entry`
-  );
+/**
+ * Get a number of documents from a given collection
+ * @param {Object} collection collection object that represents a collection in the DB
+ * @param {Object} params Query parameters to be used when getting data from DB. Optional
+ * @returns Query object to be used to retrieve the data from the DB
+ */
+var genericCollectionGet = function (collection, params = undefined) {
+  var customFilter = "";
+  var aqlParams = { };
+  let i = 0;
+
+  //Build parameters for query
+  if (params) {
+    Object.keys(params).forEach(function(key,index) {
+      if (params[key]) {
+        customFilter += `AND entry.${key} == @value${i} ` ;
+        aqlParams[`value${i}`] = params[key];
+        i += 1;
+      }
+    });
+    console.log(customFilter);
+  };
+  aqlParams['@collection'] = collection.name();
+  aqlParams.g_MaxRecordsByQuery = g_MaxRecordsByQuery;
+
+  //Build AQL query
+  var query = 'FOR entry IN @@collection FILTER entry.status != "d" ';
+  if (customFilter.length > 0) {
+    query += customFilter;
+  }
+  query += ' LIMIT @g_MaxRecordsByQuery RETURN entry';
+  console.log(query);
+  console.log(aqlParams);
+
+  //Build query object
+  const documents = db._query(query, aqlParams);
+
+  // const documents = db._query(aql`
+  //   FOR entry IN ${collection}
+  //   FILTER entry.status != "d"
+  //   LIMIT ${g_MaxRecordsByQuery}
+  //   RETURN entry`
+  // );
   return documents;
 };
 
@@ -35,7 +70,13 @@ var genericDocumentByKeyGet = function (collection, key) {
   return document;
 };
 
-
+/**
+ * Get the value od a particular field of a document from a collection by primary Key
+ * @param {Object} collection collection object that represents a collection in the DB
+ * @param {String} key document primary key
+ * @param {String} fieldName Name of the field to retrieve from the collection
+ * @returns {Object} key, field name, field value
+ */
 var genericDocumentFieldByKeyGet = function (collection, key, fieldName) {
   const document = db._query(aql`
     FOR entry IN ${collection}
@@ -47,6 +88,11 @@ var genericDocumentFieldByKeyGet = function (collection, key, fieldName) {
   return document;
 };
 
+/**
+ * Instantiate a collection object
+ * @params {String} collectionName Name of the collection that needs instantiation
+ * @returns {Object} collection Object
+ */
 var genericCollectionObjectGet = function (collectionName) {
   const collection =  db._collection(collectionName);
   if (!collection) {
@@ -55,6 +101,11 @@ var genericCollectionObjectGet = function (collectionName) {
   return collection;
 };
 
+/**
+ * Fully deletes a document from a collection  based on its primary key
+ * @param {Object} collection Object representing the collection
+ * @param {String} key Primary key of the object to be deleted
+ */
 var genericDocumentByKeyDelete = function (collection, key) {
   try {
     collection.remove(key);
@@ -67,6 +118,11 @@ var genericDocumentByKeyDelete = function (collection, key) {
   }
 };
 
+/**
+ * Logically deletes a document from a collection  based on its primary key
+ * @param {Object} collection Object representing the collection
+ * @param {String} key Primary key of the object to be logically deleted
+ */
 var genericDocumentByKeyLogicallyDelete = function (collection, key) {
   try {
     var todayDate = new Date();
@@ -81,6 +137,12 @@ var genericDocumentByKeyLogicallyDelete = function (collection, key) {
 }
 
 //TODO: Remove loop since .save handles arrays since version 3.2
+/**
+ * Post documents into a collection in the DB
+ * @param {Object} collection Object representing the collection
+ * @param {[Object]} documents Array of documents to be posted
+ * @returns {[Objects]} Posted documents
+ */
 var genericDocumentPost =  (collection, documents) => {
   const multiple = Array.isArray(documents);
   const arrayofDocuments = multiple ? documents : [documents];

beac949715effae0e576adb2cabe10b763bce14c
Juan Peredo <juan@Juans-MacBook-Pro.local>
2018-11-14 10:34:10 -0600

    Moved update mutation code to its own file as a generic function

--- Ember/app/gql/gqlHelpers/executeMutation.js
+++ Ember/app/gql/gqlHelpers/executeMutation.js
@@ -4,7 +4,7 @@ import UpdateStore from 'ember-gui/gql/gqlHelpers/updateStore';
  * Deletes a record from a detail screen by calling the DB to reomve the record and
  * then removing the record from the cache both for the detail screen and the
  * parent selection screen
- * @param {object} that Refernce to the calling object in charge of the mutation, usually a Route
+ * @param {object} that Reference to the calling object in charge of the mutation, usually a Route
  * @param {string} parentRouteName Name of the route to the parent selection screen
  * @param {string} parentQueryIdentifier Identifier used by apollo graphQL to id the query of the parent selection screen
  * @param {object} parentQueryObject Query used by the parent selection screen
@@ -12,7 +12,7 @@ import UpdateStore from 'ember-gui/gql/gqlHelpers/updateStore';
  * @param {string} queryIdentifier Identifier used by apollo graphQL to id the query of the current screen
  * @param {object} queryObject Query used by the current screen
  * @param {object} deleteMutationObject Mutation object to be used to delete the record
- * @param {object} apolloService Refernce to the apollo GraphQL service
+ * @param {object} apolloService Reference to the apollo GraphQL service
  * @returns No return value but it transitions back to the parent selection screen
  */
 function genericDeleteDataFull(that, parentRouteName, parentQueryIdentifier,
@@ -42,6 +42,52 @@ function genericDeleteDataFull(that, parentRouteName, parentQueryIdentifier,
 }
 
 
+
+
+/**
+ * Updates a record from a detail screen by calling the DB to update the record and
+ * then updating the record in the cache both for the detail screen and the
+ * parent selection screen
+ * @param {object} that Reference to the calling object in charge of the mutation, usually a Route
+ * @param {string} parentRouteName Name of the route to the parent selection screen
+ * @param {string} parentQueryIdentifier Identifier used by apollo graphQL to id the query of the parent selection screen
+ * @param {object} parentQueryObject Query used by the parent selection screen
+ * @param {string} queryIdentifier Identifier used by apollo graphQL to id the query of the current screen
+ * @param {object} queryObject Query used by the current screen
+ * @param {object} deleteMutationObject Mutation object to be used to update the record
+ * @param {object} apolloService Reference to the apollo GraphQL service
+ */
+function genericUpdateData(that,
+                          parentRouteName, parentQueryIdentifier, parentQueryObject,
+                          queryIdentifier, queryObject,
+                          updateMutationObject,apolloService){
+  //Copy the model and remove the __typename property
+  let currentModel = that.modelFor(that.routeName)[0];
+  let input = JSON.parse(JSON.stringify(currentModel));
+  let key = input._key;
+  delete input.__typename;
+  delete input._key;
+  delete input._id;
+
+  //Get the parent variables so we can update the parent query
+  let parentVariables = that.modelFor(parentRouteName) ?
+                        that.modelFor(parentRouteName)._apolloObservable.variables :
+                        null;
+
+  //Set the variables
+  let variables = { key, input};
+  //Update the backend
+  return apolloService.mutate({mutation: updateMutationObject, variables,
+    update: (store, mutationResult) => {
+
+      UpdateStore.updateRecordFromDetail(store, queryObject, key, mutationResult.data.customerPut,queryIdentifier)
+      UpdateStore.updateRecordFromSelection(store, parentQueryObject, key, mutationResult.data.customerPut, parentQueryIdentifier, parentVariables)
+
+    }
+  }, queryIdentifier)
+}
+
 export default  {
-  genericDeleteDataFull
+  genericDeleteDataFull,
+  genericUpdateData
 }
--- Ember/app/routes/customers/show.js
+++ Ember/app/routes/customers/show.js
@@ -15,30 +15,32 @@ export default Route.extend(RouteQueryManager,{
   },
   actions: {
     updateData() {
-      //Copy the model and remove the __typename property
-      let currentModel = this.modelFor(this.routeName)[0];
-      let input = JSON.parse(JSON.stringify(currentModel));
-      let key = input._key;
-      delete input.__typename;
-      delete input._key;
-      delete input._id;
-
-      //Get the parent variables so we can update the parent query
-      let parentVariables = this.modelFor('customers.index') ?
-                            this.modelFor('customers.index')._apolloObservable.variables :
-                            null;
-
-      //Set the variables
-      let variables = { key, input};
-      //Update the backend
-      return this.get("apollo").mutate({mutation, variables,
-        update: (store, mutationResult) => {
-
-          UpdateStore.updateRecordFromDetail(store, query, key, mutationResult.data.customerPut,"customer")
-          UpdateStore.updateRecordFromSelection(store, queryParent, key, mutationResult.data.customerPut, "customers", parentVariables)
-
-        }
-      }, "customer")
+      // //Copy the model and remove the __typename property
+      // let currentModel = this.modelFor(this.routeName)[0];
+      // let input = JSON.parse(JSON.stringify(currentModel));
+      // let key = input._key;
+      // delete input.__typename;
+      // delete input._key;
+      // delete input._id;
+      //
+      // //Get the parent variables so we can update the parent query
+      // let parentVariables = this.modelFor('customers.index') ?
+      //                       this.modelFor('customers.index')._apolloObservable.variables :
+      //                       null;
+      //
+      // //Set the variables
+      // let variables = { key, input};
+      // //Update the backend
+      // return this.get("apollo").mutate({mutation, variables,
+      //   update: (store, mutationResult) => {
+      //
+      //     UpdateStore.updateRecordFromDetail(store, query, key, mutationResult.data.customerPut,"customer")
+      //     UpdateStore.updateRecordFromSelection(store, queryParent, key, mutationResult.data.customerPut, "customers", parentVariables)
+      //
+      //   }
+      // }, "customer")
+      MutationHelper.genericUpdateData(this,'customers.index', "customers", queryParent,
+                                      "customer", query, mutation,this.get("apollo"))
     },
     deleteData(){
       // //Copy the model and remove the __typename property
